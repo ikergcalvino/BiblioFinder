7e9f082fe21c968179911b97ee17494148041b3c
Iker García Calviño <iker.gcalvino@udc.es>
2023-08-04 14:23:57 +0200

    Añadidas interfaces de Inicio de sesión y Registro

--- app/src/main/kotlin/com/tfg/bibliofinder/view/LoginActivity.kt
+++ app/src/main/kotlin/com/tfg/bibliofinder/view/LoginActivity.kt
@@ -1,15 +1,44 @@
 package com.tfg.bibliofinder.view
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.tfg.bibliofinder.R
+import com.tfg.bibliofinder.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
 
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        // Your login activity initialization code here
+        binding.buttonLogin.setOnClickListener {
+            val username = binding.textEmail.text.toString()
+            val password = binding.textPassword.text.toString()
+
+            if (isValidCredentials(username, password)) {
+                // Valid credentials, perform login logic here
+
+                val intent = Intent(this, MainActivity::class.java)
+                startActivity(intent)
+                finish() // Optional: Close the LoginActivity if needed
+            } else {
+                //binding.textViewErrorMessage.visibility = View.VISIBLE
+            }
+        }
+
+        // Add OnClickListener to navigate to registration activity
+        binding.createNewAccount.setOnClickListener {
+            val registrationIntent = Intent(this, RegistrationActivity::class.java)
+            startActivity(registrationIntent)
+        }
+    }
+
+    private fun isValidCredentials(username: String, password: String): Boolean {
+        // You can implement your own logic for validating credentials here
+        // For example, check against a database or predefined values
+        return username.isNotEmpty() && password.isNotEmpty()
     }
 }
--- app/src/main/kotlin/com/tfg/bibliofinder/view/RegistrationActivity.kt
+++ app/src/main/kotlin/com/tfg/bibliofinder/view/RegistrationActivity.kt
@@ -1,5 +1,37 @@
 package com.tfg.bibliofinder.view
 
+import android.content.Intent
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.tfg.bibliofinder.databinding.ActivityRegistrationBinding
 
-class RegistrationActivity : AppCompatActivity()
+class RegistrationActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityRegistrationBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityRegistrationBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Your registration activity initialization code here
+
+        // Assuming you have a "Register" button in your registration layout
+        binding.buttonRegister.setOnClickListener {
+            // Perform registration logic here
+
+            // After successful registration, call the login function
+            performLogin()
+
+            // Navigate back to the main activity
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish() // Optional: Close the RegistrationActivity if needed
+        }
+    }
+
+    private fun performLogin() {
+        // Call your login logic here
+        // You can reuse the isValidCredentials function or implement your own logic
+    }
+}
