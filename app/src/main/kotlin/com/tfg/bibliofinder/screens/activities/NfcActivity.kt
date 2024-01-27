188fa7f72a3524b47f1a4e785b48730ad28ef5e0
Iker García Calviño <iker.gcalvino@udc.es>
2024-01-27 22:28:08 +0100

    Mejoras funcionalidad NFC

--- app/src/main/kotlin/com/tfg/bibliofinder/screens/activities/NfcActivity.kt
+++ app/src/main/kotlin/com/tfg/bibliofinder/screens/activities/NfcActivity.kt
@@ -5,6 +5,7 @@ import android.app.AlertDialog
 import android.app.PendingIntent
 import android.content.Intent
 import android.content.IntentFilter
+import android.content.SharedPreferences
 import android.content.res.Configuration
 import android.nfc.NdefMessage
 import android.nfc.NfcAdapter
@@ -12,29 +13,64 @@ import android.nfc.tech.Ndef
 import android.os.Bundle
 import android.os.Handler
 import android.provider.Settings
-import android.widget.Toast
 import com.tfg.bibliofinder.R
+import com.tfg.bibliofinder.data.local.database.AppDatabase
 import com.tfg.bibliofinder.databinding.ActivityNfcBinding
+import com.tfg.bibliofinder.entities.Workstation
+import com.tfg.bibliofinder.util.MessageUtil
+import kotlinx.coroutines.DelicateCoroutinesApi
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.koin.android.ext.android.inject
 
 class NfcActivity : Activity() {
 
-    private var nfcAdapter: NfcAdapter? = null
     private lateinit var binding: ActivityNfcBinding
+
+    private var nfcAdapter: NfcAdapter? = null
+    private val database: AppDatabase by inject()
+    private val sharedPrefs: SharedPreferences by inject()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityNfcBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        val isNightMode =
-            (resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES
+        binding.nfcText.setText(R.string.scan_nfc_tag)
 
-        if (isNightMode) binding.nfcReader.setAnimation(R.raw.nfc_reader_night)
-        else binding.nfcReader.setAnimation(R.raw.nfc_reader_day)
+        if ((resources.configuration.uiMode and Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES) {
+            binding.nfcIcon.setAnimation(R.raw.nfc_reader_night)
+        } else {
+            binding.nfcIcon.setAnimation(R.raw.nfc_reader_day)
+        }
 
-        binding.nfcReader.playAnimation()
+        binding.nfcIcon.playAnimation()
 
         checkNfcStatus()
+    }
+
+    private fun checkNfcStatus() {
+        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
+
+        if (nfcAdapter == null) {
+            MessageUtil.showToast(applicationContext, getString(R.string.unsupported_nfc_device))
+            finish()
+        }
+
+        if (!nfcAdapter!!.isEnabled) {
+            val builder = AlertDialog.Builder(this)
+            builder.setTitle(getString(R.string.nfc_disabled))
+            builder.setMessage(getString(R.string.enable_nfc))
+            builder.setPositiveButton(getString(R.string.settings)) { _, _ ->
+                val intent = Intent(Settings.ACTION_WIRELESS_SETTINGS)
+                startActivity(intent)
+            }
+            builder.setNegativeButton(getString(R.string.cancel)) { _, _ -> finish() }
+            builder.setCancelable(false)
+            builder.show()
+        }
     }
 
     override fun onResume() {
@@ -46,65 +82,68 @@ class NfcActivity : Activity() {
             Intent(this, javaClass).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP),
             PendingIntent.FLAG_MUTABLE
         )
+
         val intentFiltersArray = arrayOf(IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED))
         val techListsArray = arrayOf(arrayOf(Ndef::class.java.name))
+
         nfcAdapter?.enableForegroundDispatch(
             this, pendingIntent, intentFiltersArray, techListsArray
         )
     }
 
-    private fun checkNfcStatus() {
-        nfcAdapter = NfcAdapter.getDefaultAdapter(this)
-        if (nfcAdapter == null) {
-            Toast.makeText(this, "Este dispositivo no admite NFC", Toast.LENGTH_SHORT).show()
-            finish()
-        }
-        if (!nfcAdapter!!.isEnabled) {
-            showNfcEnableDialog()
-        }
-    }
-
-    private fun showNfcEnableDialog() {
-        val builder = AlertDialog.Builder(this)
-        builder.setTitle("NFC desactivado")
-        builder.setMessage("Para utilizar esta aplicación, debes habilitar NFC en tu dispositivo.")
-        builder.setPositiveButton("Configuración") { _, _ ->
-            val intent = Intent(Settings.ACTION_WIRELESS_SETTINGS)
-            startActivity(intent)
-        }
-        builder.setNegativeButton("Cancelar") { _, _ -> finish() }
-        builder.setCancelable(false)
-        builder.show()
-    }
-
+    @OptIn(DelicateCoroutinesApi::class)
     override fun onNewIntent(intent: Intent) {
         super.onNewIntent(intent)
+
+        val loggedInUserId = sharedPrefs.getLong("userId", 0L)
+
         if (intent.action == NfcAdapter.ACTION_TECH_DISCOVERED) {
             val rawMessages = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)
+
             if (rawMessages != null) {
                 val messages = arrayOfNulls<NdefMessage>(rawMessages.size)
                 for (i in rawMessages.indices) {
                     messages[i] = rawMessages[i] as NdefMessage
                 }
+
                 if (messages.isNotEmpty()) {
                     val records = messages[0]?.records
+
                     if (!records.isNullOrEmpty()) {
                         val payload = records[0].payload
 
                         val payloadWithoutHeader = payload.copyOfRange(3, payload.size)
-                        val text = String(payloadWithoutHeader, Charsets.UTF_8)
+                        val workstationId =
+                            String(payloadWithoutHeader, Charsets.UTF_8).toLongOrNull()
 
-                        binding.nfcReader.setAnimation(R.raw.nfc_success)
-                        binding.nfcReader.playAnimation()
+                        GlobalScope.launch(Dispatchers.Main) {
+                            val workstation = withContext(Dispatchers.IO) {
+                                workstationId?.let {
+                                    database.workstationDao().getWorkstationById(it)
+                                }
+                            }
 
-                        binding.nfcReader.setAnimation(R.raw.nfc_fail)
-                        binding.nfcReader.playAnimation()
+                            if (workstation?.state == Workstation.WorkstationState.AVAILABLE || (workstation?.state == Workstation.WorkstationState.BOOKED && workstation.userId == loggedInUserId)) {
+                                workstation.state = Workstation.WorkstationState.OCCUPIED
+                                workstation.userId = loggedInUserId
 
-                        Handler().postDelayed({
-                            val intentMain = Intent(this, MainActivity::class.java)
-                            startActivity(intentMain)
-                            finish()
-                        }, 5000)
+                                database.workstationDao().updateWorkstation(workstation)
+
+                                binding.nfcText.setText(R.string.workstation_occupied)
+                                binding.nfcIcon.setAnimation(R.raw.nfc_success)
+                            } else {
+                                binding.nfcText.setText(R.string.workstation_not_occupied)
+                                binding.nfcIcon.setAnimation(R.raw.nfc_fail)
+                            }
+
+                            binding.nfcIcon.playAnimation()
+
+                            Handler().postDelayed({
+                                val intentMain = Intent(this@NfcActivity, MainActivity::class.java)
+                                startActivity(intentMain)
+                                finish()
+                            }, 2500)
+                        }
                     }
                 }
             }
