4e1bdad4407d29e4186aae2b1b4fef1a0391841c
Iker García Calviño <iker.gcalvino@udc.es>
2024-02-03 18:11:23 +0100

    Añadidas constantes

---
+++ app/src/main/kotlin/com/tfg/bibliofinder/util/Constants.kt
@@ -0,0 +1,9 @@
+package com.tfg.bibliofinder.util
+
+object Constants {
+    const val USER_ID = "userId"
+    const val USER_NAME = "userName"
+    const val USER_EMAIL = "userEmail"
+    const val EMAIL_REGEX = "^[A-Za-z0-9+_.-]+@(.+)\$"
+    const val PASSWORD_REGEX = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).{12,}\$"
+}
